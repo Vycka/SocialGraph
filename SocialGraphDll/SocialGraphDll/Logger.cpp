9825c16d048bde14f3bd3f99271d9cd648ddeb3d
Vyckaxxx <Vyckaxxx@2e9c4e4c-5ef7-11de-a55d-e76e375ae706>
2009-07-02 20:39:43 +0000

    Temporal solution to avoid corruptin log file (need to do with queue buffer in the future)

--- SocialGraphDll/SocialGraphDll/Logger.cpp
+++ SocialGraphDll/SocialGraphDll/Logger.cpp
@@ -1,13 +1,17 @@
 #include "Logger.h"
 #include "Node.h"
 #include "Edge.h"
+#include <sstream> //quick workaround for messing logs
 #include <time.h>
+
 
 Logger::Logger(const char *f)
 {
 	log.open(f,std::ios_base::out | std::ios_base::app);
 	int t = (int)time(NULL);
-	log << t << ' ' << VID_INIT << std::endl; 
+	std::stringstream ss;
+	ss << t << ' ' << VID_INIT << std::endl;
+	log << ss.str(); 
 }
 
 Logger::~Logger()
@@ -18,43 +22,58 @@ Logger::~Logger()
 void Logger::wFrame(int frame)
 {
 	int t = (int)time(NULL);
-	log << t << ' ' << VID_SETFRAME << ' ' << frame << std::endl; 
+	std::stringstream ss;
+	ss << t << ' ' << VID_SETFRAME << ' ' << frame << std::endl; 
+	log << ss.str();
 }
 
 void Logger::wAddNode(Node *n, double weight)
 {
 	int t = (int)time(NULL);
-	log << t << ' ' << VID_ADDNODE << ' ' << *n->getNick() << ' ' << weight << std::endl;
+	std::stringstream ss;
+	ss << t << ' ' << VID_ADDNODE << ' ' << *n->getNick() << ' ' << weight << std::endl;
+	log << ss.str();
 }
 
 void Logger::wDelNode(Node *n)
 {
+	
 	int t = (int)time(NULL);
-	log << t << ' ' << VID_DELETENODE << ' ' << *n->getLNick() << std::endl; 
+	std::stringstream ss;
+	ss << t << ' ' << VID_DELETENODE << ' ' << *n->getLNick() << std::endl; 
+	log << ss.str();
 }
 
 void Logger::wAddEdge(Edge *e,double weight)
 {
 	int t = (int)time(NULL);
-	log << t << ' ' << VID_ADDEDGE << ' ' << *e->getSource()->getLNick() << ' '
+	std::stringstream ss;
+	ss << t << ' ' << VID_ADDEDGE << ' ' << *e->getSource()->getLNick() << ' '
 		<< *e->getTarget()->getLNick() << ' ' << weight << ' '
 		<< e->getActivityTime() << std::endl; 
+	log << ss.str();
 }
 
 void Logger::wEnd()
 {
 	int t = (int)time(NULL);
-	log << t << ' ' << VID_CLEAR << std::endl; 
+	std::stringstream ss;
+	ss << t << ' ' << VID_CLEAR << std::endl; 
+	log << ss.str();
 }
 
 void Logger::wPause()
 {
 	int t = (int)time(NULL);
-	log << t << ' ' << VID_PAUSE << std::endl; 
+	std::stringstream ss;
+	ss << t << ' ' << VID_PAUSE << std::endl; 
+	log << ss.str();
 }
 
 void Logger::wResume()
 {
 	int t = (int)time(NULL);
-	log << t << ' ' << VID_RESUME << std::endl; 
+	std::stringstream ss;
+	ss << t << ' ' << VID_RESUME << std::endl; 
+	log << ss.str();
 }
